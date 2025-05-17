86946aa777f8c91d58d88e98b6a9a9b15541278a
row-huh <rohaabropathan@gmail.com>
2025-05-18 04:14:23 +0500

    .

--- script.py
+++ script.py
@@ -1,12 +1,59 @@
 import argparse
 
 
+
+def addItem(item):
+    '''Function to handle adding new item into the lst'''
+    #TODO
+
+
+def deleteItem(itemID):
+    '''Function to delete any item given its id'''
+    #TODO
+
+
+def listAllItems():
+    '''lists all existing items along with their ids'''
+    #TODO
+
+
+def nextTask():
+    '''Show the next task on the list(usually the first task)
+    Maybe use cowsay coz ive waited my entire life to use that library'''
+    #TODO
+
+
 def main():
-    parser = argparse.ArgumentParser(description="A simple CLI tool")
 
-    parser.add_argument('--addTask', type=str, help='Use this flag along with a string to add a new task!')
-    parser.add_argument('--deleteTask', type=int, help='Enter an existing id of a task to delete it')
+    #main parser
+    parser = argparse.ArgumentParser(description='CLI tool to keep track of your immediate cli tasks')
 
-    parser.add_argument('--listAll', type="None", help='Lists all existing items in the task list')
+    subparsers = parser.add_subparsers(dest='command', help='commands to run')
 
-    # separate functions for each
+    add_parser = subparsers.add_parser('addItem', help='command to add items to the list')
+
+    add_parser.add_argument('addItem', type=str, help='Use this flag along with a string to add a new task!')
+
+
+    delete_parser = subparsers.add_parser('deleteItem', help='delete items from list')
+    delete_parser.add_argument('deleteItem', type=int, help='enter id of the item to be deleted along with the flag')
+
+
+    # listAllItems = subparsers.add_parser()
+    # add in arguments to list item
+    # maybe add in some sorting or grep like thingy
+    # like get all items with a string in them etc etc
+
+
+    # separate functions for each 
+
+    args = parser.parse_args()
+
+
+    if args.command == 'addItem':
+        print('function called here')
+        addItem(args.name, args.count)
+
+
+if __name__ == '__main__':
+    main()
