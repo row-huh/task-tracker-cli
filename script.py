26d2ca9f31594f46cc008a79e62d589e2384e806
row-huh <rohaabropathan@gmail.com>
2025-05-19 10:34:45 +0500

    implemented addtask cmds

--- script.py
+++ script.py
@@ -1,15 +1,31 @@
 import argparse
+import csv
 
 
+FILENAME = 'tasks.csv'
 
-def addItem(item):
+def addItem(arg):
     '''Function to handle adding new item into the lst'''
-    #TODO
+    item = arg.addItem
+
+    last_id = int(get_last_row_id())
+    new_id = last_id + 1
+
+    # read all existing rows (there's no append thing - writer will start writing from line 1)
+    with open(FILENAME, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        all_rows = list(reader)
+
+    # write all existing rows first, then add the new row
+    with open(FILENAME, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(all_rows)
+        writer.writerow((new_id,item)) 
 
 
 def deleteItem(itemID):
     '''Function to delete any item given its id'''
-    #TODO
+    #TODO 
 
 
 def listAllItems():
@@ -27,11 +43,9 @@ def main():
 
     #main parser
     parser = argparse.ArgumentParser(description='CLI tool to keep track of your immediate cli tasks')
-
     subparsers = parser.add_subparsers(dest='command', help='commands to run')
 
     add_parser = subparsers.add_parser('addItem', help='command to add items to the list')
-
     add_parser.add_argument('addItem', type=str, help='Use this flag along with a string to add a new task!')
 
 
@@ -52,7 +66,29 @@ def main():
 
     if args.command == 'addItem':
         print('function called here')
-        addItem(args.name, args.count)
+        addItem(args)
+
+
+def get_last_row_id():
+    file_path = 'tasks.csv'
+
+    try:
+        with open(file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            rows = list(reader)
+
+            if rows:
+                row = rows[-1]
+                id = row[0]
+                print(id)
+                return id
+            else:
+                return -1
+            
+    except FileNotFoundError:
+        raise Exception("Error: File not found at path")
+    except Exception as e:
+        raise Exception("An error occurred: {e}")
 
 
 if __name__ == '__main__':
